f89b174ba8b7f56dcc7c5edceb664eced40e97da
Jensenleung2465 <121221992+Jensenleung2465@users.noreply.github.com>
2023-04-26 20:21:58 +0800

    source file

---
+++ main.py
@@ -0,0 +1,20 @@
+#copyright by givemetocode, nocopyright.(c)2023-4-2309:42pm
+#first
+print('givemetocode infounder applicion v1.0beta - source verision1')
+print(' 請勿使用此應用程式做非法的東西')
+loopcount = 0
+while (loopcount < 9):
+    user = input('name: ')
+    if user == " " or user == "" or user == "" or user == " " or user == " " or user == " ":
+        print('Last Name: ')
+        print('First Name: ')
+        print("wechat id:")
+        print("school email: ")
+    if user == "" or user == "" or user == "" or user == " " or user == " " or user == " ":
+        print('Last Name: ')
+        print('First Name: ')
+        print("wechat id:   ")
+        print("school email:   ")
+        print('Phone_Number: +  ')
+    else :
+        print("請你輸入他/她的英文名 ( 第一個字一定要大寫 )")
